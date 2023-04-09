7d326653582eb20e1ca5a5f8ddc278d556dd3cf2
Real Matrix <noel.ar@protonmail.com>
2023-04-09 06:26:06 +0100

    Update test.cs

--- test.cs
+++ test.cs
@@ -16,7 +16,8 @@ public abstract class RepositoryBase<TE, TR> where TE : class where TR : DbConte
 	}
 	
 	#region implementation
-	public virtual async Task<IEnumerable<TE>> FirstOrDefaultAsync(Expression<Func<TE, bool>> predicate, Func<IQueryable<TE>, IIncludableQueryable<TE, object>> include = null, bool disableTracking = true)
+	public virtual async Task<IEnumerable<TE>> FirstOrDefaultAsync(Expression<Func<TE, bool>> predicate, 
+		Func<IQueryable<TE>, IIncludableQueryable<TE, object>> include = null, bool disableTracking = true)
     {
         IQueryable<TE> query = _dbSet;
         if (disableTracking != null) query = query.AsNoTracking(); 
