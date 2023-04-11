97d0aea6d205ae94aa892aa95063754be19a5a23
Real Matrix <noel.ar@protonmail.com>
2023-04-11 08:28:48 +0100

    Update test.cs

--- test.cs
+++ test.cs
@@ -8,20 +8,29 @@ public abstract class RepositoryBase<TE, TR> where TE : class where TR : DbConte
 	private IDbFactory<TR> DbFactory { get; set }  //props
 	protected TR InitContext => _localContext ?? (_localContext = DbFactory.Init());
 	#endregion
-	protected RepositoryBase(IDbFactory<TR> dbFactory) 
+	protected RepositoryBase(IDbFactory<TR> dbFactory) //injecting context-specific dbFactory 
 	{
 		DbFactory = dbFactory;
 		_dbSet = InitContext.Set<TE>();
 	}
 	#region implementation
-	public virtual async Task<IEnumerable<TE>> FirstOrDefaultAsync(Expression<Func<TE, bool>> predicate, Func<IQueryable<TE>, IIncludableQueryable<TE, object>> 
-		include = null, bool disableTracking = true)
+	public virtual async Task<IEnumerable<TE>> FirstOrDefaultAsync(Expression<Func<TE, bool>> predicate, Func<IQueryable<TE>,
+		IIncludableQueryable<TE, object>> include=null, bool disableTracking=true)
    	{
         	IQueryable<TE> query = _dbSet;
         	if (disableTracking) query = query.AsNoTracking(); 
         	if (include != null) query = include(query);
 		return await query.FirstOrDefaultAsync(predicate);
     	}
+	public virtual void Delete(TE entity)
+	{
+		_dbSet.Remove(entity);
+	}
+	public virtual void Delete(Expression<Func<TE, bool>> predicate)
+	{
+		IEnumerable<TE> objects = _dbSet.Where(predicate).AsEnumerable();
+		foreach (obj in objects) _dbSet.Remove(obj);
+	}
 	#endregion
 }
  
