a92915e326c8b65d9752b0ed710d34ce4d5f3424
Real Matrix <noel.ar@protonmail.com>
2023-04-13 05:03:04 +0100

    Update Scaffold_DbContextInstance.cs

--- Scaffold_DbContextInstance.cs
+++ Scaffold_DbContextInstance.cs
@@ -15,25 +15,22 @@ namespace // i.e. NoelsWhiteboard.Api.Context
 	public class ReboxContext : DbContext
 	{
 		public string ConnectionString { get; set; }
-		public ReboxContext()
-		{
-		}
+		public ReboxContext() {}
 		public ReboxContext(DbContextOptions<ReboxContext> options) : base(options) {}
-		public ReboxContext(string connectionString) : base()
-		{
-			ConnecitonString = connectionString;
-		}
+		public ReboxContext(string connectionString) : base() {ConnecitonString=connectionString;}
+		
 		#region sets
 		public virtual DbSet<Comp> Comps { get; set; }
 		public virtual DbSet<User> Users { get; set; }
 		public virtual DbSet<Entry> Entries { get; set; }
 		#endregion
+		
 		protected override void OnConfiguring(DbContextOptionsBuilder options)
 		{
 			options.UseSqlServer(ConnectionString, x => x.MigrationsAssembly("NoelsWhiteboard.Api.Migrations"));
 			base.OnConfiguring(optionsBuilder);
 		}
-
+		
 		public override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<User>(entity => entitiy.Property(x => x.Id).HasColumnName("ID"));
