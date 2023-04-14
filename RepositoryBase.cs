29556153b1d1ca0026e6e365f64bb04e4124173c
Real Matrix <noel.ar@protonmail.com>
2023-04-14 06:48:55 +0100

    Update RepositoryBase.cs

--- RepositoryBase.cs
+++ RepositoryBase.cs
@@ -9,16 +9,12 @@ namespace // i.e. NoelsWhiteboard.Api.Context.Infrastructure
 		private IDbFactory<TR> DbFactory { get; set; }  //props
 		protected TR InitContext => _localContext ?? (_localContext = DbFactory.Init());
 		#endregion
-		protected RepositoryBase(IDbFactory<TR> dbFactory) //injecting context-specific dbFactory 
+		protected RepositoryBase(IDbFactory<TR> dbFactory) //injecting IDbFactory instance 
 		{
 			DbFactory = dbFactory ?? throw new ArgumentNullException(nameof(dbFactory));
 			_dbSet = InitContext.Set<TE>();
 		}
 		#region implementation
-		public virtual IQueryable<IGrouping<int, TE>> GroupBy(Expression<Func<TE, int>> keySelector)
-       		{
-            		return _dbSet.GroupBy(keySelector);
-       		}
 		public virtual async Task<TE?> FirstOrDefaultAsync(Expression<Func<TE, bool>> predicate, Func<IQueryable<TE>, IIncludableQueryable<TE, object>>
 			include = null, bool disableTracking = true)
 		{
@@ -27,6 +23,10 @@ namespace // i.e. NoelsWhiteboard.Api.Context.Infrastructure
 			if (include != null) query = include(query);
 			return await query.FirstOrDefaultAsync(predicate);
 		}
+		public virtual IQueryable<IGrouping<int, TE>> GroupBy(Expression<Func<TE, int>> keySelector)
+       		{
+            		return _dbSet.GroupBy(keySelector);
+       		}
 		public virtual void Delete(TE entity)
 		{
 			_dbSet.Remove(entity);
