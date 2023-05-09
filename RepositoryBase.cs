80cdd938ea1ad78e5c1ec9410d9363f9bd74a536
Real Matrix <noel.ar@protonmail.com>
2023-05-09 13:54:11 +0800

    Update RepositoryBase.cs

--- RepositoryBase.cs
+++ RepositoryBase.cs
@@ -15,12 +15,6 @@ namespace // i.e. NoelsWhiteboard.DAL.Infrastructure
 			_dbSet = InitContext.Set<TE>(); 
 		}
 		#region implementation
-		public virtual IQueryable<TE?> GetAllAsQueryable(Func<IQueryable<TE>, IIncludableQueryable<TE, object>>? include = null, bool disableTracking = true)
-        	{
-          		 IQueryable<TE> query = _dbSet;
-         	         if (disableTracking) query = query.AsNoTracking();
-         		 return include != null ? include(query) : query;
-     		}
 		public virtual async Task<TE?> FirstOrDefaultAsync(Expression<Func<TE, bool>> predicate, Func<IQueryable<TE>, 
 			IIncludableQueryable<TE,object>>? include = null, bool disableTracking = true)
 		{
@@ -29,6 +23,12 @@ namespace // i.e. NoelsWhiteboard.DAL.Infrastructure
 			if (include != null) query = include(query);
 			return await query.FirstOrDefaultAsync(predicate);
 		}
+		public virtual IQueryable<TE?> GetAllAsQueryable(Func<IQueryable<TE>, IIncludableQueryable<TE, object>>? include = null, bool disableTracking = true)
+        	{
+          		 IQueryable<TE> query = _dbSet;
+         	         if (disableTracking) query = query.AsNoTracking();
+         		 return include != null ? include(query) : query;
+     		}
 		public virtual IQueryable<IGrouping<int, TE>> GroupBy(Expression<Func<TE, int>> keySelector)
        		{
             		return _dbSet.GroupBy(keySelector);
