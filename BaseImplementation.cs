a0485889fdff096eac5a9fd3c209f8219b15e148
Real Matrix <noel.ar@protonmail.com>
2023-11-27 15:43:14 +0000

    Update BaseImplementation.cs

--- BaseImplementation.cs
+++ BaseImplementation.cs
@@ -17,7 +17,7 @@ public abstract partial class RepositoryBase<TE, TR, TL> where TE : class, new()
         (_dbSet, _logger) = (InitContext.Set<TE>(), logger ?? NullArg<ILogger<TL>>(logger!));
     }
     #region [implementation]
-    public virtual async Task<bool> AnyAsync(Expression<Func<TE, bool>> predicate) => await _dbSet.AnyAsync(predicate);
+    public virtual IQueryable<IGrouping<int, TE>> GroupBy(Expression<Func<TE, int>> keySelector) => _dbSet.GroupBy(keySelector);
     public virtual async Task<IEnumerable<TE?>> GetManyAsync(Expression<Func<TE, bool>> predicate, Func<IQueryable<TE>, IIncludableQueryable<TE, object>>? include = null, bool disableTracking = true)
     {
         IQueryable<TE> query = _dbSet;
@@ -25,7 +25,7 @@ public abstract partial class RepositoryBase<TE, TR, TL> where TE : class, new()
         if (include != null) query = include(query);
         return await query.Where(predicate).ToListAsync();
     }
-    public virtual IQueryable<IGrouping<int, TE>> GroupBy(Expression<Func<TE, int>> keySelector) => _dbSet.GroupBy(keySelector);
+    public virtual async Task<bool> AnyAsync(Expression<Func<TE, bool>> predicate) => await _dbSet.AnyAsync(predicate);
     public virtual IEnumerable<TE?> GetAll(Func<IQueryable<TE>, IIncludableQueryable<TE, object>>? include = null, bool disableTracking = true)
     {
         IQueryable<TE> query = _dbSet;
