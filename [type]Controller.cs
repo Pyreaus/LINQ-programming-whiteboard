ff1a0aee46f28d552c9865cba02ed73dfdb0a211
Real Matrix <noel.ar@protonmail.com>
2024-01-02 15:30:21 +0000

    Update and rename Generic-Controller.cs to [type]Controller.cs

--- [type]Controller.cs
+++ [type]Controller.cs
@@ -31,6 +31,7 @@ public sealed partial class UserController : ControllerBase
         IEnumerable<Skill?> skills = await _diaryService.GetSkills();
         return (skills != null) && (typeof(List<Skill>) == skills!.GetType()) ? Ok(skills) : StatusCode(204);
     }
+    
     /// <summary>
     /// GET: {{host}}/api/{{version}}/User/GetTraineesByReviewer/[pfid]
     /// </summary>
@@ -54,6 +55,28 @@ public sealed partial class UserController : ControllerBase
         foreach (PeopleFinderUser? user in users) user!.Photo = (bnetUrl + user.Photo?.ToString()) ?? "../../../assets/profilePic.png";
         foreach (TraineeViewModel? trainee in traineesVM) _mapper.Map(users.FirstOrDefault(user => trainee?.TRAINEE_PFID == user?.PFID.ToString())!, trainee);
         return (trainees.GetType() == typeof(List<Trainee>)) && traineesVM != null ? Ok(traineesVM) : StatusCode(500);
+    }
+
+    /// <summary>
+    /// PUT: {{host}}/api/{{version}}/User/SetPair/[pfid]
+    /// </summary>
+    /// <param name="pfid">PFID of trainee</param>
+    /// <param name="addReq">request DTO</param>
+    /// <response code="500">internal error</response>
+    /// <response code="400"><see cref="Trainee"/> not modified</response>
+    /// <response code="201"><see cref="Trainee"/> modified</response>
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status201Created,Type=typeof(TraineeViewModel))]
+    [ActionName("SetPair"),Authorize(Policy="admin"),HttpPut("[action]/{pfid:int}")]
+    public async Task<ActionResult<TraineeViewModel>?> SetPair([FromRoute] [ValidPfid] int pfid, [FromBody] AddModifyTraineeReq addReq)
+    {
+        Trainee? currentTrainee = await _userService.GetTraineeByPfidAsync(pfid);
+        if ((currentTrainee is null)||(addReq is null)||(await _userService.GetPFUserAsync(pfid) is null)) return StatusCode(204);
+        _userService.SetPair(_mapper.Map(addReq, currentTrainee!));
+        TraineeViewModel traineeVM = _mapper.Map<Trainee,TraineeViewModel>(currentTrainee!);
+        return traineeVM != null ? CreatedAtAction(nameof(GetTraineesByReviewer), new { pfid = currentTrainee?.REVIEWER_PFID }, traineeVM) : StatusCode(500);
     }
 
     /// <summary>
@@ -107,28 +130,6 @@ public sealed partial class UserController : ControllerBase
             }
          }
         return env.IsDevelopment() ? throw new Exception() : StatusCode(StatusCodes.Status500InternalServerError);
-    }
-
-    /// <summary>
-    /// PUT: {{host}}/api/{{version}}/User/SetPair/[pfid]
-    /// </summary>
-    /// <param name="pfid">PFID of trainee</param>
-    /// <param name="addReq">request DTO</param>
-    /// <response code="500">internal error</response>
-    /// <response code="400"><see cref="Trainee"/> not modified</response>
-    /// <response code="201"><see cref="Trainee"/> modified</response>
-    [Consumes(MediaTypeNames.Application.Json)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    [ProducesResponseType(StatusCodes.Status201Created,Type=typeof(TraineeViewModel))]
-    [ActionName("SetPair"),Authorize(Policy="admin"),HttpPut("[action]/{pfid:int}")]
-    public async Task<ActionResult<TraineeViewModel>?> SetPair([FromRoute] [ValidPfid] int pfid, [FromBody] AddModifyTraineeReq addReq)
-    {
-        Trainee? currentTrainee = await _userService.GetTraineeByPfidAsync(pfid);
-        if ((currentTrainee is null)||(addReq is null)||(await _userService.GetPFUserAsync(pfid) is null)) return StatusCode(204);
-        _userService.SetPair(_mapper.Map(addReq, currentTrainee!));
-        TraineeViewModel traineeVM = _mapper.Map<Trainee,TraineeViewModel>(currentTrainee!);
-        return traineeVM != null ? CreatedAtAction(nameof(GetTraineesByReviewer), new { pfid = currentTrainee?.REVIEWER_PFID }, traineeVM) : StatusCode(500);
     }
     
     /// <summary>
